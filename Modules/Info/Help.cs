9f636b20a4e70095f7a5a27ae946912a08e46659
9Tails <53715423+9TailsDev@users.noreply.github.com>
2021-12-02 16:07:30 +0800

    Add files via upload

---
+++ Modules/Info/Help.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azukoto.Modules.Info
+{
+    public class Help : ModuleBase
+    {
+        [Command("help")]
+        public async Task Helpasync()
+        {
+            var embed = new EmbedBuilder()
+                .WithTitle("Help Menu")
+                .WithThumbnailUrl("https://cdn.discordapp.com/app-icons/798526044134834186/fd80098ece933f2ee79a379c5ecdbe8d.png?size=256")
+                .AddField("Fun", "Ping", false)
+                .AddField("Admin", "HAHAH PEDO", false)
+                .AddField("Info", "Info <user>", false)
+                .WithColor(new Color(252, 255, 84))
+                .Build();
+
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
---
+++ Services/CHandler.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzukotoBot.Services
+{
+        public class CHandler : DiscordClientService
+            {
+                private readonly IServiceProvider provider;
+                private readonly DiscordSocketClient client;
+                private readonly CommandService service;
+                private readonly IConfiguration configuration;
+                private readonly ILogger<DiscordClientService> logger;
+
+        public CHandler(DiscordSocketClient client, ILogger<DiscordClientService> logger, IServiceProvider provider, CommandService service, IConfiguration configuration) 
+            : base(client, logger)
+        {
+                this.provider = provider;
+                this.client = client;
+                this.service = service;
+                this.configuration = configuration;
+                this.logger = logger;
+        }
+
+            protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+            {
+                this.client.MessageReceived += OnMessageReceved;
+                this.service.CommandExecuted += CommandExecuted;
+                await this.service.AddModulesAsync(Assembly.GetEntryAssembly(), this.provider);
+            }
+
+            private async Task CommandExecuted(Optional<CommandInfo> commandInfo, ICommandContext commandContext, IResult result)
+            {
+                if (result.IsSuccess)
+                {
+                    return;
+                }
+
+                await commandContext.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+            private async Task OnMessageReceved(SocketMessage socketMessage)
+            {
+                if (!(socketMessage is SocketUserMessage message)) return;
+                if (message.Source != MessageSource.User) return;
+
+                var argPos = 0;
+                if (!message.HasStringPrefix(this.configuration["Prefix"], ref argPos) && !message.HasMentionPrefix(this.client.CurrentUser, ref argPos)) return;
+
+                var context = new SocketCommandContext(this.client, message);
+                await this.service.ExecuteAsync(context, argPos, this.provider);
+        }
+    }
+}
